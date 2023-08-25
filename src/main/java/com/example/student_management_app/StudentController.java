e80f8811d39e7964bdf96b33ef8b074b553e2eb6
unknown <kalpeshpatil19999@gmail.com>
2023-08-25 23:29:34 +0530

    student_management_app

--- src/main/java/com/example/student_management_app/StudentController.java
+++ src/main/java/com/example/student_management_app/StudentController.java
@@ -1,7 +1,11 @@
 package com.example.student_management_app;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -9,6 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("Student")
+@Component
 public class StudentController {
 
     @Autowired
@@ -16,7 +21,8 @@ public class StudentController {
 
     @GetMapping("/get")
     public Student getStudent(@RequestParam("q") int regNO){
-       return studentService.getStudent(regNO);
+
+        return studentService.getStudent(regNO);
     }
 
     @PostMapping("/add")
@@ -25,9 +31,14 @@ public class StudentController {
 
     }
 
-    @PostMapping("/getbypath{id}")
-    public Student getStudentbypath(@PathVariable("id") int regNO){
-       return studentService.getStudentbypath(regNO);
+    @PostMapping("/getbypath/{id}")
+    public ResponseEntity getStudentbypath(@PathVariable("id") int regNO){
+
+       Student student= studentService.getStudentbypath(regNO);
+       if(student==null) {
+           return new ResponseEntity("id does not exits", HttpStatus.NOT_FOUND);
+       }
+       return new ResponseEntity(student,HttpStatus.FOUND);
     }
 
     @PutMapping("updateAge/{id}")
